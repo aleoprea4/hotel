799954ad6fb6f9100b62d74401bcc8b92091b5a7
aleoprea4 <alexandru.oprea.fmi@gmail.com>
2023-06-07 00:53:41 +0300

    final v2

--- manager/rooms.php
+++ manager/rooms.php
@@ -58,6 +58,7 @@ if (!$_SESSION['admin_ses']) { //if user not logged in, we do not show page
                 <img src='../media/$img' alt='Background'>
                 <h4>Room type: $class_name</h4>
                 <p>$description</p> <br>
+                <p>$id_room</p> <br>
                 <a href='roomanager.php?ourtext=$id_room'>Make changes to this room</a><br>
               </div>";
             }
@@ -94,11 +95,11 @@ if (!$_SESSION['admin_ses']) { //if user not logged in, we do not show page
     <h3>Create a new room</h3>
     <form action="" method="post">
        <!-- <input type="text" name="id" value="<--?php echo $id; ?>"> -->
-        <input type="text" name="firn" value="<?php echo $fir; ?>">
-        <input type="text" name="lan" value="<?php echo $lan; ?>">
-        <input type="text" name="biy" value="<?php echo $by; ?>">
-        <input type="text" name="addr" value="<?php echo $addr; ?>">
-        <input type="text" name="desc" value="<?php echo $desc; ?>">
+        <input type="text" name="firn" value="<?php echo $fir; ?>" placeholder="Numar camera">
+        <input type="text" name="lan" value="<?php echo $lan; ?>" placeholder="Clasa camera">
+        <input type="text" name="biy" value="<?php echo $by; ?>"    placeholder="Capacitate">
+        <input type="text" name="addr" value="<?php echo $addr; ?>" placeholder="Pret">
+        <input type="text" name="desc" value="<?php echo $desc; ?>"placeholder="Descriere">
 
 
 
